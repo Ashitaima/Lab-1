73f4f37d5e94d8a12173d6c7e4457c1a4179c843
Roman Melnichuk <roman.melnichuk.2000@gmail.com>
2024-09-24 23:12:39 +0300

    lab 3 first commit

---
+++ Task03_01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+class Person
+{
+    private string name;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public Person()
+    {
+        name = "Unknown";
+        age = 0;
+    }
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Person[] people = new Person[3];
+        for (int i = 0; i < people.Length; i++)
+        {
+            Console.WriteLine($"Введіть ім'я для людини {i + 1}: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Введіть вік для людини {i + 1}: ");
+            int age = int.Parse(Console.ReadLine());
+
+            people[i] = new Person(name, age);
+        }
+        foreach (var person in people)
+        {
+            person.Display();
+
+        }
+    }
+}
---
+++ Task03_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+class Person
+{
+    private string name;
+    private int age;
+
+    public Person() : this("No name", 1)
+    { }
+    public Person(int age) : this("No name", age)
+    { }
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Person[] people = new Person[3];
+        for (int i = 0; i < people.Length; i++)
+        {
+            Console.WriteLine($"Введіть ім'я для людини {i + 1}: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Введіть вік для людини {i + 1}: ");
+            int age = int.Parse(Console.ReadLine());
+
+            people[i] = new Person(name, age);
+        }
+        foreach (var person in people)
+        {
+            person.Display();
+
+        }
+    }
+}
